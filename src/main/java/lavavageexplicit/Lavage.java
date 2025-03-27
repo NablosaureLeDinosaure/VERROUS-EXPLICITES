6dcb0d4332f92082beaf36c93b7379c8d8aa55e7
NablosaureLeDinosaure <albazzou.nabil@gmail.com>
2025-03-27 14:46:16 +0100

    final push

--- src/main/java/lavavageexplicit/Lavage.java
+++ src/main/java/lavavageexplicit/Lavage.java
@@ -1,36 +1,29 @@
-package lavavageexplicit;
+package lavage;
 
-import java.util.*;
 import java.io.*;
 
 public class Lavage {
 
 	public static void main(String[] args) {
 		// La ressource partagée
-		Pile pileAssiettes = new Pile();
+		PileDAssiettes pileAssiettes = new PileDAssiettes();
 		// Les threads qui se partagent la ressource
-		Laveur l = new Laveur(pileAssiettes);
-		Laveur l2 = new Laveur(pileAssiettes);
-		Essuyeur e = new Essuyeur(pileAssiettes);
-		//Essuyeur e2 = new Essuyeur(pileAssiettes);
-		//Essuyeur e3 = new Essuyeur(pileAssiettes);
+		Laveur l = new Laveur(pileAssiettes); l.start();
+		Laveur l2 = new Laveur(pileAssiettes); l2.start();
+		Laveur l3 = new Laveur(pileAssiettes); l3.start();
+		Essuyeur e = new Essuyeur(pileAssiettes); e.start();
+		Essuyeur e2 = new Essuyeur(pileAssiettes); e2.start();
+		//Essuyeur e3 = new Essuyeur(pileAssiettes); e3.start();
 
-		l.start();
-		e.start();
-		l2.start();
-		//e2.start();
-		// e3.start();
-		// attendre la frappe d’une touche dans la console
+		// Arrête le programme lors de la frappe d’une touche dans la console
 		try {
 			System.in.read();
-		} catch (IOException ex) {
-		}
-
+		} catch (IOException ex) {}
 		l.interrupt();
-		e.interrupt();
 		l2.interrupt();
+		l3.interrupt();
+		e.interrupt();
 		//e2.interrupt();
 		//e3.interrupt();
-
 	}
 }
